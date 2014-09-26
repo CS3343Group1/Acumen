0a2a07bebf2576a0ec3e7db2d3028d7e6947af18
ERIC <xikanfeng2@gamil.com>
2014-09-26 10:42:39 +0800

    change valid name

---
+++ raw_data/SourceCode/io/IO.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class IO{
+	private String[] words;
+	private String infile;
+	private String infolder;
+
+	public IO(Sting infolder){
+
+	}
+
+	public IO(String infile){
+		
+	}
+}
